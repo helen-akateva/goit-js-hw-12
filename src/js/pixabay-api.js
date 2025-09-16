b2f6d70c18dcfecda63b21d1b7210eefe40a3f9a
helen-akateva <akareva17@gmail.com>
2025-09-16 14:14:42 +0300

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,18 +5,17 @@ const API_KEY = '52251600-fd545eaf39516c2667db2508b';
 
 axios.defaults.baseURL = BASE_URL;
 
-export function getImagesByQuery(query) {
-  return axios
-    .get('', {
-      params: {
-        key: API_KEY,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      },
-    })
-    .then(response => {
-      return response.data;
-    });
+export async function getImagesByQuery(query, page) {
+  const response = await axios.get('', {
+    params: {
+      key: API_KEY,
+      q: query,
+      page,
+      per_page: 15,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+  return response.data;
 }
--- src/main.js
+++ src/main.js
@@ -6,53 +6,109 @@ import {
   clearGallery,
   showLoader,
   hideLoader,
+  showLoadMoreButton,
+  hideLoadMoreButton,
 } from './js/render-functions';
 
 const formEl = document.querySelector('form');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let page = 1;
+const perPage = 15;
+let inputQuery = null;
 
 formEl.addEventListener('submit', handleFormSubmit);
+loadMoreBtn.addEventListener('click', handleClick);
 
-function handleFormSubmit(event) {
+async function handleFormSubmit(event) {
   event.preventDefault();
+  page = 1;
+  hideLoadMoreButton();
+  clearGallery();
+  showLoader();
 
-  const inputQuery = event.currentTarget.elements['search-text'].value.trim();
+  inputQuery = event.currentTarget.elements['search-text'].value.trim();
 
   if (!inputQuery) {
     iziToast.warning({
       message: 'Please enter a search term!',
+      position: 'topRight',
+      messageColor: '#fff',
+      backgroundColor: '#ef4040',
+      icon: '',
     });
     return;
   }
-  clearGallery();
-  showLoader();
+  try {
+    const res = await getImagesByQuery(inputQuery, page);
 
-  getImagesByQuery(inputQuery)
-    .then(response => {
-      const photosArray = response.hits;
-
-      if (photosArray.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-          messageColor: '#fff',
-          backgroundColor: '#ef4040',
-          icon: '',
-        });
-        return;
-      }
-
-      createGallery(photosArray);
-    })
-    .catch(error => {
-      console.log(error);
+    if (res.hits.length === 0) {
       iziToast.error({
-        title: 'Error',
-        message: 'Something went wrong. Please try again!',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+        messageColor: '#fff',
+        backgroundColor: '#ef4040',
+        icon: '',
       });
-    })
-    .finally(() => {
-      hideLoader();
-      formEl.reset();
+      return;
+    }
+
+    createGallery(res.hits);
+    scrollPage();
+
+    if (res.totalHits > perPage) {
+      showLoadMoreButton();
+    }
+  } catch (error) {
+    console.log(error);
+    iziToast.error({
+      position: 'topRight',
+      message: 'Something went wrong. Please try again!',
+      messageColor: '#fff',
+      backgroundColor: '#ef4040',
+      icon: '',
     });
+  } finally {
+    hideLoader();
+    formEl.reset();
+  }
+}
+
+async function handleClick() {
+  showLoader();
+  page += 1;
+
+  try {
+    const res = await getImagesByQuery(inputQuery, page);
+    createGallery(res.hits);
+
+    const lastPage = Math.ceil(res.totalHits / perPage);
+
+    if (page === lastPage) {
+      hideLoadMoreButton();
+      iziToast.info({
+        message: 'Were sorry, but you ve reached the end of search results.',
+        messageColor: '#fff',
+        backgroundColor: ' #59a10d',
+        position: 'bottomRight',
+        icon: '',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoader();
+  }
+}
+
+function scrollPage() {
+  const galleryItem = document.querySelector('.gallery-item');
+  if (galleryItem) {
+    const itemHeight = galleryItem.getBoundingClientRect().height;
+    window.scrollBy({
+      top: itemHeight * 2,
+      behavior: 'smooth',
+    });
+  }
 }
