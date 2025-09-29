32eb8f7cfe533b462e46fdfaff8c0a2ed8bc2b02
helen-akateva <akateva17@gmail.com>
2025-09-29 15:15:40 +0300

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,21 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const BASE_URL = 'https://pixabay.com/api/';
-// const API_KEY = '52251600-fd545eaf39516c2667db2508b';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '52251600-fd545eaf39516c2667db2508b';
 
-// axios.defaults.baseURL = BASE_URL;
+axios.defaults.baseURL = BASE_URL;
 
-// export async function getImagesByQuery(query, page) {
-//   const response = await axios.get('', {
-//     params: {
-//       key: API_KEY,
-//       q: query,
-//       page,
-//       per_page: 15,
-//       image_type: 'photo',
-//       orientation: 'horizontal',
-//       safesearch: true,
-//     },
-//   });
-//   return response.data;
-// }
+export async function getImagesByQuery(query, page) {
+  const response = await axios.get('', {
+    params: {
+      key: API_KEY,
+      q: query,
+      page,
+      per_page: 15,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+  return response.data;
+}
